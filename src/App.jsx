88957dd96c79fe2ee371fd45a6ccca9537d6b592
Kcin054 <nickpatiphat001@gmail.com>
2024-03-19 11:20:18 +0700

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState('');
+  const [data, setData] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const handleCheck = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    if (editIndex !== -1) {
+      const newData = [...data];
+      newData[editIndex] = inputValue;
+      setData(newData);
+      setEditIndex(-1);
+      setInputValue('');
+    } else {
+      setData([...data, inputValue]);
+      setInputValue('');
+    }
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setInputValue(data[index]);
+  };
+
+  const handleRemove = (index) => {
+    const newData = data.filter((_,i) => i !== index);
+    setData(newData);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type='text' value={inputValue} onChange={handleCheck}></input>
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
+
+      <ul>
+        {data.map((data, index) => (
+          <li key={index}>
+            {data}
+            <button onClick={() => handleEdit(index)}>Edit</button>
+            <button onClick={() => handleRemove(index)}>Delete</button>
+          </li>
+          
+        ))}
+      </ul>
+    </div>
     </>
   )
 }
