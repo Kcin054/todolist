51434a546ef74dd4727f654fb056b195d30b6d18
Kcin054 <nickpatiphat001@gmail.com>
2024-03-21 11:18:53 +0700

    commit day4

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Button, Card, Input } from "antd";
+import SizeContext from "antd/es/config-provider/SizeContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [data, setData] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (editIndex !== -1) {
+      const newData = [...data];
+      newData[editIndex] = input;
+      setData(newData);
+      setEditIndex(-1);
+      setInput("");
+    } else {
+      setData([...data, input]);
+      setInput("");
+    }
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setInput(data[index]);
+  };
+
+  const handleRemove = (index) => {
+    const newData = data.filter((_, i) => i !== index);
+    setData(newData);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          value={input}
+          onChange={handleChange}
+          style={{ width: 300, margin:10 }}
+        ></Input>
+        <Button onClick={handleSubmit}>Submit</Button>
+      </form>
+        {data.map((data, index) => (
+          <Card key={index}>
+            {data}
+            <Button onClick={() => handleEdit(index)} style={{margin:5}}>Edit</Button>
+            <Button onClick={() => handleRemove(index)} style={{margin:5}}>Delete</Button>
+          </Card>
+        ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
