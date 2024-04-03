e086edbe2626602949c4739e0d63f25f839e0edc
Kcin054 <nickpatiphat001@gmail.com>
2024-04-03 16:33:32 +0700

    todolist w4d3

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Input from 'antd/es/input/Input'
+import InputValue from './Input'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Input/>
+      <InputValue/>
     </>
   )
 }
